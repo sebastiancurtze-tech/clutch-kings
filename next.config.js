765d7caaf4a341f0327177f68a78518fbd077625
sebastiancurtze-tech <sebastian.curtze@gmail.com>
2025-10-24 14:23:50 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+      "@/firebase": path.resolve(__dirname, "src/firebase"),
+      "@/lib": path.resolve(__dirname, "src/lib"),
+      "@/hooks": path.resolve(__dirname, "src/hooks"),
+      "@/components": path.resolve(__dirname, "src/components"),
+    };
     return config;
   },
 };
